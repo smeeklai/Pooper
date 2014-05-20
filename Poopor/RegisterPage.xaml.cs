a161e073fd07ccac522ec2ff0219fd8d88f08cb2
smeeklai <wizer_boss@hotmail.com>
2014-05-20 17:27:12 +0700

    Finished getting report v2

--- Poopor/RegisterPage.xaml.cs
+++ Poopor/RegisterPage.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using Poopor.Resources;
 using System.Net.NetworkInformation;
 using System.Threading.Tasks;
-using AmCharts.Windows.QuickCharts;
 
 namespace Poopor
 {
